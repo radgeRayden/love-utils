8fb128831bb2e6d2b60fea549e947aa13e9d5063
Westerbly Snaydley <westerbly@snaydley.com>
2015-10-30 02:56:36 -0500

    New versions
    Debug Overlay:
    - fps counter now only updates every second, improving readability.
    
    Vector2:
    - Now has a copy() instance method, which returns a new vector with the
    same values
    - Instance methods are now assigned on new() instead of parsed every
    time

--- debugoverlay.lua
+++ debugoverlay.lua
@@ -4,19 +4,24 @@ This is licensed under the MIT license (http://opensource.org/licenses/MIT).
 Time of creation: September 9th, 2015 20:59 ACT
 Description:
 Simple overlay for displaying debug information (only fps as of this version).
-Version: 1.00
+Version: 1.0.1 (October 30th, 2015)
 --]]
 
 overlay = {}
 
-local fps = 0
+local fps = 60
+local timer = 0
 
 function overlay.draw()
   love.graphics.print('FPS: ' .. fps, 25, 25)
 end
 
 function overlay.update(dt)
-  fps = 1 / dt - (1 / dt % 1)
+  timer = timer + dt
+  if timer > 1 then
+    fps = 1 / dt - (1 / dt % 1)
+    timer = timer % 1
+  end
 end
 
 return overlay
